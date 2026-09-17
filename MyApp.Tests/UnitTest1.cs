b39479c2090fe4e7dbbd39c28e4053465ba124bb
George Robinson <george@socialhootz.com>
2026-09-17 12:42:16 +1000

    added unit test

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Intentionally failing assertion
+        Assert.Equal(4, 2 + 2); // Change this to Assert.Equal(5, 2 + 2);
+    }
+}
