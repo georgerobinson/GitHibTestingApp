206997ec8e173f49a58175ad8dd0df53482f9d5d
George Robinson <george@socialhootz.com>
2026-09-19 14:07:12 +1000

    Added a 2nd unit 2

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -8,4 +8,11 @@ public class UnitTest1
         // Intentionally failing assertion
         Assert.Equal(4, 2 + 2); // Change this to Assert.Equal(5, 2 + 2);
     }
+
+    [Fact]
+    public void Test2()
+    {
+        // Intentionally failing assertion
+        Assert.Equal(6, 3 + 3); // Change this to Assert.Equal(5, 2 + 2);
+    }
 }
